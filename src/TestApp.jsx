94d0cb311ef7754acb9cf6a368176a5f0dcf6ea2
Akhil636 <akhiakhil636@gmail.com>
2026-03-28 22:55:10 +0530

    first commit

---
+++ src/TestApp.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1 style={{ color: "red" }}>✅ React is working!</h1>
+      <p>If you can see this, React is rendering correctly.</p>
+    </div>
+  );
+}
